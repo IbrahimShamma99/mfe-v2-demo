dab57cd45227ee02faca0ce2963c144a033f1c6a
Ibrahim Shamma <i.abushammah@gmail.com>
2024-12-28 20:33:50 +0300

    run the app

--- apps/header/src/Header.tsx
+++ apps/header/src/Header.tsx
@@ -1,4 +1,4 @@
-export function FallbackHeader() {
+export default function Header() {
   return (
     <div>
       <h1>Main Header</h1>
--- apps/main/src/App.tsx
+++ apps/main/src/App.tsx
@@ -1,6 +1,13 @@
+import { FallbackHeader } from "./FallbackHeader";
+import { Suspense } from "react";
+import Header from "header/header";
+
 const App = () => {
   return (
     <div className="content">
+      <Suspense fallback={<FallbackHeader />}>
+        <Header />
+      </Suspense>
       <h1>Main App</h1>
     </div>
   );
